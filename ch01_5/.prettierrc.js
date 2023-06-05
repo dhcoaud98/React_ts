6a41d0892344629a2bfff83971f9037cb081bb93
dhcoaud <dhcoaud98@gmail.com>
2023-06-05 15:16:39 +0900

    ch01_5

---
+++ ch01_5/.prettierrc.js
@@ -0,0 +1,9 @@
+module.export = {
+  bracketSpacing: false,
+  jsxBracketSameLine: true,
+  singleQuote: true,
+  trailingComma: 'none',
+  arrowParens: 'avoid',
+  semi: false,
+  pringWidth: 90
+}
